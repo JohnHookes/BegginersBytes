a1143d7a3a4600845ae6ef628cd7c52cad865bcb
John Hooks <johnhookes@aol.com>
2024-08-15 16:31:37 +0000

    connected database to code, env.py, settings.py databases and install django databases

--- beginnersbytes/settings.py
+++ beginnersbytes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-w71906@d@$e2z$3yd##rjuh*vc&2n%fn8hbgqo+mkl0+4#aa_k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-johnhookes-beginnersbyt-x8s5g9wc5pr.ws.codeinstitute-ide.net', '8000-johnhookes-beginnersbyt-irswoifzt0x.ws-eu115.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-johnhookes-beginnersbyt-x8s5g9wc5pr.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -74,11 +78,15 @@ WSGI_APPLICATION = 'beginnersbytes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 CSRF_TRUSTED_ORIGINS = [
